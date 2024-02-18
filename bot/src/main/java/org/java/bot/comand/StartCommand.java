f24c5b784c50f0d4dd43647d694254d32007168a
Mary Okhtienko <mariohtienko@gmail.com>
2024-02-18 12:22:45 +0300

    Add StartCommand

---
+++ bot/src/main/java/org/java/bot/comand/StartCommand.java
@@ -0,0 +1,50 @@
+package org.java.bot.comand;
+
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.java.bot.aspect.BotCommand;
+import org.java.bot.bot.Command;
+import org.java.bot.service.UserService;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class StartCommand implements Command {
+    private static final String COMMAND = "/start";
+    private static final String START_DESCRIPTION = "register a user";
+    private static final String HELP_MESSAGE = "Type /help to view the list of commands.";
+
+    private final UserService userService;
+
+    @Override
+    public String command() {
+        return COMMAND;
+    }
+
+    @Override
+    public String description() {
+        return START_DESCRIPTION;
+    }
+
+    @BotCommand
+    @Override
+    public SendMessage handle(Update update) {
+        Long chatId = update.message().chat().id();
+        StringBuilder message = new StringBuilder();
+        Long userId = update.message().from().id();
+
+        if (userService.isExists(userId)) {
+            log.info("User {} is already registered.", userId);
+            message.append("This user is already registered and has access to all bot features. " + HELP_MESSAGE);
+        } else {
+            userService.save(userId);
+            log.info("User {} registered successfully.", userId);
+            message.append("Welcome! You are now registered and have access to all bot features. " + HELP_MESSAGE);
+        }
+
+        return new SendMessage(chatId, message.toString());
+    }
+}

fc9937257db93b49ffd9a667e4e227cb3ecf0232
Mary Okhtienko <mariohtienko@gmail.com>
2024-02-28 01:00:18 +0300

    Add ClientConfiguration for managing API URLs

---
+++ scrapper/src/main/java/org/java/scrapper/configuration/ClientConfiguration.java
@@ -0,0 +1,39 @@
+package org.java.scrapper.configuration;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+@ConfigurationProperties(prefix = "client")
+@Data
+public class ClientConfiguration {
+    private final String DEFAULT_GITHUB_URL = "https://api.github.com";
+    private final String DEFAULT_STACKOVERFLOW_URL = "https://api.stackexchange.com/2.3";
+    private String githubBaseUrl;
+    private String stackOverflowBaseUrl;
+
+    @Bean(name = "gitHubWebClient")
+    public WebClient gitHubWebClient() {
+        String url = githubBaseUrl != null || githubBaseUrl.isEmpty()
+            ? githubBaseUrl
+            : DEFAULT_GITHUB_URL;
+
+        return WebClient.builder()
+            .baseUrl(url)
+            .build();
+    }
+
+   @Bean(name = "stackOverflowWebClient")
+    public WebClient stackOverflowWebClient() {
+       String url = githubBaseUrl != null || stackOverflowBaseUrl.isEmpty()
+           ? stackOverflowBaseUrl
+           : DEFAULT_STACKOVERFLOW_URL;
+
+        return WebClient.builder()
+            .baseUrl(url)
+            .build();
+    }
+}

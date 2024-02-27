f3c9892de430c556e7d27b1cfb21a1b1c8177e71
Mary Okhtienko <mariohtienko@gmail.com>
2024-02-27 15:27:31 +0300

    Update BotApplication

--- bot/src/main/java/org/java/bot/BotApplication.java
+++ bot/src/main/java/org/java/bot/BotApplication.java
@@ -1,14 +1,27 @@
 package org.java.bot;
 
+import lombok.RequiredArgsConstructor;
 import org.java.bot.configuration.ApplicationConfig;
+import org.java.bot.handler.LinkTrackerBot;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy
+@RequiredArgsConstructor
 @SpringBootApplication
 @EnableConfigurationProperties(ApplicationConfig.class)
-public class BotApplication {
+public class BotApplication implements CommandLineRunner {
+    private final LinkTrackerBot linkTrackerBot;
+
     public static void main(String[] args) {
         SpringApplication.run(BotApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        linkTrackerBot.start();
     }
 }

96a53517be96c3ef688557e7636f6421d339d248
Mary Okhtienko <mariohtienko@gmail.com>
2024-02-28 15:01:45 +0300

    Update ApplicationConfig

--- scrapper/src/main/java/org/java/scrapper/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/org/java/scrapper/configuration/ApplicationConfig.java
@@ -1,13 +1,17 @@
-package org.java.configuration;
+package org.java.scrapper.configuration;
 
 import jakarta.validation.constraints.NotNull;
 import java.time.Duration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
+@EnableScheduling
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
 public record ApplicationConfig(
+    @Bean
     @NotNull
     Scheduler scheduler
 ) {

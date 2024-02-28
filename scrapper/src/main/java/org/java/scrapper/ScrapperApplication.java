35193f85c3e8ca2cde16b8657a32b9851ad7930a
Mary Okhtienko <mariohtienko@gmail.com>
2024-02-28 15:13:59 +0300

    Update ScrapperApplication

--- scrapper/src/main/java/org/java/scrapper/ScrapperApplication.java
+++ scrapper/src/main/java/org/java/scrapper/ScrapperApplication.java
@@ -1,6 +1,6 @@
-package org.java;
+package org.java.scrapper;
 
-import org.java.configuration.ApplicationConfig;
+import org.java.scrapper.configuration.ApplicationConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;

9b0b4c2023e04f32bae0288e55c9d58732389021
Mary Okhtienko <mariohtienko@gmail.com>
2024-03-03 22:40:05 +0300

    Rename BotConfiguration to ServiceConfiguration

--- scrapper/src/main/java/org/java/scrapper/configuration/ServiceConfiguration.java
+++ scrapper/src/main/java/org/java/scrapper/configuration/ServiceConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class BotConfiguration {
+public class ServiceConfiguration {
     @Value("${service.bot-base-url}")
     private String scrapperBaseUrl;
 

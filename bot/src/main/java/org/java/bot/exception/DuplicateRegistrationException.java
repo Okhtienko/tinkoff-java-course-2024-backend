f9411217facd26415dfe1dd0ac36444197da9fd1
Mary Okhtienko <mariohtienko@gmail.com>
2024-03-03 22:02:47 +0300

    Remove unused exception classes

--- bot/src/main/java/org/java/bot/exception/DuplicateRegistrationException.java
+++
@@ -1,9 +0,0 @@
-package org.java.bot.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class DuplicateRegistrationException extends ApiException {
-    public DuplicateRegistrationException(String message) {
-        super(message, HttpStatus.CONFLICT);
-    }
-}
--- bot/src/main/java/org/java/bot/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package org.java.bot.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class ResourceNotFoundException extends ApiException {
-    public ResourceNotFoundException(String message) {
-        super(message, HttpStatus.BAD_REQUEST);
-    }
-}

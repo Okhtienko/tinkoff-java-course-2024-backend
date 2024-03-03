9782cc9cfa29af7a68e8e108cc25870399b22e00
Mary Okhtienko <mariohtienko@gmail.com>
2024-03-03 22:08:48 +0300

    Add LinkService for managing links

---
+++ scrapper/src/main/java/org/java/scrapper/service/LinkService.java
@@ -0,0 +1,113 @@
+package org.java.scrapper.service;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.java.scrapper.dto.LinkResponse;
+import org.java.scrapper.exception.ChatNotFoundException;
+import org.java.scrapper.exception.DuplicateChatRegistrationException;
+import org.java.scrapper.exception.InvalidRequestException;
+import org.java.scrapper.exception.LinkNotFoundException;
+import org.java.scrapper.repository.LinkRepository;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class LinkService implements LinkRepository {
+    private Map<Long, List<LinkResponse>> links;
+
+    public LinkService() {
+        this.links = new HashMap<>();
+    }
+
+    @Override
+    public void register(Long id) {
+        validateChat(id);
+        if (links.containsKey(id)) {
+            throw new DuplicateChatRegistrationException("Chat already registered");
+        }
+        links.put(id, new ArrayList<>());
+    }
+
+    @Override
+    public void delete(Long id) {
+        validateChat(id);
+        if (!links.containsKey(id)) {
+            throw new ChatNotFoundException("Chat not found");
+        }
+        links.remove(id);
+    }
+
+    @Override
+    public List<LinkResponse> gets(Long id) {
+        log.info("Retrieving links for chat ID: {}", id);
+        if (!links.containsKey(id)) {
+            throw new InvalidRequestException("Invalid Chat ID");
+        }
+        return links.get(id);
+    }
+
+    @Override
+    public LinkResponse save(Long id, String url) throws URISyntaxException {
+        log.info("Saving link for chat ID: {} with URL: {}", id, url);
+        validateUrl(url);
+        LinkResponse link = new LinkResponse().setId(1L).setUrl(new URI(url));
+
+        links.computeIfPresent(id, (key, links) -> {
+            links.add(link);
+            return links;
+        });
+
+        return link;
+    }
+
+    @Override
+    public LinkResponse remove(Long id, String url) throws URISyntaxException {
+        log.info("Removing link for chat ID: {} with URL: {}", id, url);
+        validateUrl(url);
+
+        if (!exists(id, url)) {
+            throw new LinkNotFoundException("Link not found");
+        }
+
+        LinkResponse link = new LinkResponse().setId(1L).setUrl(new URI(url));
+
+        links.computeIfPresent(id, (key, links) -> {
+            links.add(link);
+            return links;
+        });
+
+        return link;
+    }
+
+    private void validateChat(Long id) {
+        if (id == null) {
+            throw new InvalidRequestException("Invalid request parameters: Chat ID cannot be null");
+        }
+    }
+
+    private void validateUrl(String url) {
+        if (!checkLinkFormat(url)) {
+            throw new InvalidRequestException("Invalid request parameters");
+        }
+    }
+
+    private Boolean checkLinkFormat(String url) {
+        String regex = "^(https?://)?([a-zA-Z0-9]+[a-zA-Z0-9-]*\\.)+[a-zA-Z]{2,6}(/.*)?$";
+        return url.matches(regex);
+    }
+
+    private Boolean exists(Long id, String url) {
+        return links.get(id).stream().anyMatch(link -> {
+            try {
+                return link.getUrl().equals(new URI(url));
+            } catch (URISyntaxException e) {
+                return false;
+            }
+        });
+    }
+}

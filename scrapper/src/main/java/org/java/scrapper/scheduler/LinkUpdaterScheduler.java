472bcc661c80283e75bdcb3be4336315025aca31
Mary Okhtienko <mariohtienko@gmail.com>
2024-03-04 15:45:03 +0300

    Update LinkUpdaterScheduler

--- scrapper/src/main/java/org/java/scrapper/scheduler/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/org/java/scrapper/scheduler/LinkUpdaterScheduler.java
@@ -1,11 +1,13 @@
 package org.java.scrapper.scheduler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@ConditionalOnProperty(prefix = "app.scheduler", name = "enable", havingValue = "true")
 public class LinkUpdaterScheduler {
     @Scheduled(fixedDelayString = "#{@scheduler.interval}")
     public void update() {

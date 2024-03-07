9d6260eeda38bcc34c49b9375aa447064ba5dd3c
Mary Okhtienko <mariohtienko@gmail.com>
2024-03-08 00:11:00 +0300

    Update UpdateService

--- bot/src/main/java/org/java/bot/service/UpdateService.java
+++ bot/src/main/java/org/java/bot/service/UpdateService.java
@@ -2,7 +2,6 @@ package org.java.bot.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.java.bot.client.ScrapperClient;
 import org.java.bot.dto.LinkUpdateRequest;
 import org.java.bot.repository.UpdateRepository;
 import org.springframework.stereotype.Service;
@@ -11,16 +10,16 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UpdateService implements UpdateRepository {
-    private final ScrapperClient scrapperClient;
 
     @Override
     public void sendUpdate(LinkUpdateRequest request) {
+        log.info("Received update request: {}", request);
         validateRequest(request);
     }
 
     private void validateRequest(LinkUpdateRequest request) {
         if (request.getId() == null || request.getUrl() == null) {
-            throw new IllegalArgumentException("Required fields (id, url) are missing");
+            throw new IllegalArgumentException("Invalid request parameters");
         }
     }
 }

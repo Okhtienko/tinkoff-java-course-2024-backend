f901e5e5b520948154bc3385cc923ba948de990a
Mary Okhtienko <mariohtienko@gmail.com>
2024-03-08 13:27:14 +0300

    Update BotClient

---
+++ scrapper/src/main/java/org/java/scrapper/client/BotClient.java
@@ -0,0 +1,27 @@
+package org.java.scrapper.client;
+
+import org.java.scrapper.dto.LinkUpdateRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+public class BotClient {
+    private static final String DEFAULT_BASE_URL = "http://localhost:8090/api/v1/bot";
+    private final WebClient webClient;
+
+    public BotClient(@Value("${service.bot-base-url}") String botBaseUrl) {
+        this.webClient = WebClient.builder()
+            .baseUrl(botBaseUrl == null || botBaseUrl.isEmpty() ? DEFAULT_BASE_URL : botBaseUrl)
+            .build();
+    }
+
+    public Mono<String> sendUpdate(LinkUpdateRequest request) {
+        return webClient.post()
+            .uri("/updates")
+            .bodyValue(request)
+            .retrieve()
+            .bodyToMono(String.class);
+    }
+}

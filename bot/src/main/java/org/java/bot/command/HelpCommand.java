4622ba9d7d003b38663118e857cd2e9a9dce611e
Mary Okhtienko <mariohtienko@gmail.com>
2024-03-10 20:23:39 +0300

    Update HelpCommand

--- bot/src/main/java/org/java/bot/command/HelpCommand.java
+++ bot/src/main/java/org/java/bot/command/HelpCommand.java
@@ -5,10 +5,12 @@ import com.pengrad.telegrambot.request.SendMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.java.bot.aspect.BotCommand;
 import org.java.bot.bot.Command;
-import org.java.bot.handler.CommandHandler;
+import org.java.bot.component.CommandHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -16,11 +18,11 @@ public class HelpCommand implements Command {
     private static final String COMMAND = "/help";
     private static final String HELP_DESCRIPTION = "display help menu";
 
-    private CommandHandler handler;
+    private CommandHolder commands;
 
     @Autowired
-    public HelpCommand(@Lazy CommandHandler handler) {
-        this.handler = handler;
+    public HelpCommand(@Lazy CommandHolder commands) {
+        this.commands = commands;
     }
 
     @Override
@@ -39,9 +41,15 @@ public class HelpCommand implements Command {
         Long chatId = update.message().chat().id();
         log.info("Processing help command for chat ID: {}", chatId);
 
-        String message = handler.toStringCommands();
+        String message = toStringCommands(commands.gets());
 
         log.info("Sending help message to chat ID: {}", chatId);
         return new SendMessage(chatId, message);
+    }
+
+    private String toStringCommands(Map<String, Command> commands) {
+        return commands.entrySet().stream()
+            .map(entry -> entry.getKey() + " - " + entry.getValue().description())
+            .collect(Collectors.joining("\n", "Available commands:\n\n", ""));
     }
 }

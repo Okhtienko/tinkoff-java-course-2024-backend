92358fff6715fc8e18e2bbb0716715acb22f646d
Mary Okhtienko <mariohtienko@gmail.com>
2024-03-10 21:10:51 +0300

    Update UntrackCommand

--- bot/src/main/java/org/java/bot/command/UntrackCommand.java
+++ bot/src/main/java/org/java/bot/command/UntrackCommand.java
@@ -2,11 +2,13 @@ package org.java.bot.command;
 
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.java.bot.aspect.BotCommand;
 import org.java.bot.bot.Command;
 import org.java.bot.service.BotStateService;
+import org.java.bot.service.LinkService;
 import org.java.bot.util.BotState;
 import org.springframework.stereotype.Component;
 
@@ -18,6 +20,7 @@ public class UntrackCommand implements Command {
     private static final String UNTRACK_DESCRIPTION = "stop tracking a link";
 
     private final BotStateService botStateService;
+    private final LinkService linkService;
 
     @Override
     public String command() {
@@ -34,9 +37,18 @@ public class UntrackCommand implements Command {
     public SendMessage handle(Update update) {
         Long chatId = update.message().chat().id();
         log.info("Handling UntrackCommand for chat ID: {}", chatId);
+        List<String> links = linkService.get(chatId);
+
+        if (check(links)) {
+            return new SendMessage(chatId, "There are no tracked links currently. ");
+        }
 
         botStateService.set(BotState.WAITING_FOR_LINK_REMOVE);
 
         return new SendMessage(chatId, "Enter untracking link:");
+    }
+
+    private boolean check(List<String> links) {
+        return links == null || links.isEmpty();
     }
 }

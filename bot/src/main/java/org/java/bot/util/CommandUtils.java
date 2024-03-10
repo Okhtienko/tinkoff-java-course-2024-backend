8ecc842b809c4b0613376ced480a79d1b4a92752
Mary Okhtienko <mariohtienko@gmail.com>
2024-03-10 20:37:13 +0300

    Implement CommandUtils for common command logic

---
+++ bot/src/main/java/org/java/bot/util/CommandUtils.java
@@ -0,0 +1,19 @@
+package org.java.bot.util;
+
+import java.util.Set;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public final class CommandUtils {
+    private static final  Set<String> COMMANDS = Set.of("/start", "/help");
+
+    private CommandUtils() {
+
+    }
+
+    public static Boolean belong(String command) {
+        Boolean exists = COMMANDS.contains(command);
+        log.info("Command {} exists: {}", command, exists);
+        return exists;
+    }
+}

42fc436216988027f6bdd86a077465024af56a8c
Mary Okhtienko <mariohtienko@gmail.com>
2024-03-10 20:47:57 +0300

    Update UserService

--- bot/src/main/java/org/java/bot/service/UserService.java
+++ bot/src/main/java/org/java/bot/service/UserService.java
@@ -1,9 +1,7 @@
 package org.java.bot.service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
 import org.java.bot.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -11,28 +9,22 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class UserService implements UserRepository {
-    private Map<Long, List<String>> users;
+    private Set<Long> users;
 
     public UserService() {
-        this.users = new HashMap<>();
+        this.users = new HashSet<>();
     }
 
     @Override
     public void save(Long id) {
         log.info("Adding user: {}", id);
-        users.put(id, new ArrayList<>());
+        users.add(id);
     }
 
     @Override
     public Boolean exists(Long id) {
-        boolean exists = users.containsKey(id);
+        boolean exists = users.contains(id);
         log.info("User with id {} exists: {}", id, exists);
         return exists;
-    }
-
-    @Override
-    public Map<Long, List<String>> get() {
-        log.info("Retrieving all users and their links");
-        return users;
     }
 }

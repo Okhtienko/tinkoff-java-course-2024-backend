2c0d1cbbba48b65f77fc1b0dc61801c912718e1c
Mary Okhtienko <mariohtienko@gmail.com>
2024-03-14 21:00:30 +0300

    Add Validation and ResponseStatus

--- scrapper/src/main/java/org/java/scrapper/controller/ScrapperController.java
+++ scrapper/src/main/java/org/java/scrapper/controller/ScrapperController.java
@@ -1,12 +1,13 @@
 package org.java.scrapper.controller;
 
+import jakarta.validation.Valid;
 import java.net.URISyntaxException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.java.scrapper.dto.LinkRequest;
 import org.java.scrapper.dto.LinkResponse;
 import org.java.scrapper.service.LinkService;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,33 +25,35 @@ public class ScrapperController {
     private final LinkService linkService;
 
     @PostMapping("/{id}")
-    public ResponseEntity<String> register(@PathVariable Long id) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public String register(@PathVariable Long id) {
         linkService.register(id);
-        return ResponseEntity.ok().body("Registration successful");
+        return "Registration successful";
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> delete(@PathVariable Long id) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
         linkService.delete(id);
-        return ResponseEntity.ok().body("Deletion successful");
     }
 
     @GetMapping("/links")
-    public List<LinkResponse> gets(@RequestHeader("Tg-Chat-Id") Long chatId) {
+    public List<String> gets(@RequestHeader("Tg-Chat-Id") Long chatId) {
         return linkService.gets(chatId);
     }
 
     @PostMapping("/links")
     public LinkResponse save(
         @RequestHeader("Tg-Chat-Id") Long id,
-        @RequestBody LinkRequest request) throws URISyntaxException {
+        @RequestBody @Valid LinkRequest request) throws URISyntaxException {
         return linkService.save(id, request.getUrl());
     }
 
     @DeleteMapping("/links")
-    public LinkResponse remove(
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void remove(
         @RequestHeader("Tg-Chat-Id") Long id,
-        @RequestBody LinkRequest request) throws URISyntaxException {
-        return linkService.remove(id, request.getUrl());
+        @RequestBody  @Valid LinkRequest request) throws URISyntaxException {
+        linkService.remove(id, request.getUrl());
     }
 }

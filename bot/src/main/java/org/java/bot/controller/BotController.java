a137c4d3ad17d1c3da24fb1c34f8b79672ca21cf
Mary Okhtienko <mariohtienko@gmail.com>
2024-03-14 20:49:33 +0300

    Add Validation and ResponseStatus

--- bot/src/main/java/org/java/bot/controller/BotController.java
+++ bot/src/main/java/org/java/bot/controller/BotController.java
@@ -1,12 +1,14 @@
 package org.java.bot.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.java.bot.dto.LinkUpdateRequest;
 import org.java.bot.service.UpdateService;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,8 +18,9 @@ public class BotController {
     private final UpdateService updateService;
 
     @PostMapping("/updates")
-    public ResponseEntity<String> sendUpdate(@RequestBody LinkUpdateRequest request) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public String sendUpdate(@RequestBody @Valid LinkUpdateRequest request) {
         updateService.sendUpdate(request);
-        return ResponseEntity.ok().body("Update processed successfully");
+        return "Update processed successfully";
     }
 }

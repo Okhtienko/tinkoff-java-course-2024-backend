85d7fb04ad05ac6b2ef7d6775b4f7ff469061fdb
Mary Okhtienko <mariohtienko@gmail.com>
2024-03-18 11:54:49 +0300

    Update ChatRepository

--- scrapper/src/main/java/org/java/scrapper/repository/ChatRepository.java
+++ scrapper/src/main/java/org/java/scrapper/repository/ChatRepository.java
@@ -1,6 +1,7 @@
 package org.java.scrapper.repository;
 
 import java.util.List;
+import java.util.Optional;
 import org.java.scrapper.dto.chat.ChatRequest;
 import org.java.scrapper.dto.chat.ChatResponse;
 
@@ -9,7 +10,7 @@ public interface ChatRepository {
 
     void delete(Long chatId);
 
-    ChatResponse get(Long id);
+    Optional<ChatResponse> get(Long chatId);
 
     List<ChatResponse> gets();
 }

10fb19c868e1e59b6b2f09ac8b234bd31d71589a
Mary Okhtienko <mariohtienko@gmail.com>
2024-03-18 11:57:25 +0300

    Update ChatController

--- scrapper/src/main/java/org/java/scrapper/controller/ChatController.java
+++ scrapper/src/main/java/org/java/scrapper/controller/ChatController.java
@@ -2,6 +2,7 @@ package org.java.scrapper.controller;
 
 import jakarta.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.java.scrapper.dto.chat.ChatRequest;
 import org.java.scrapper.dto.chat.ChatResponse;
@@ -22,21 +23,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatController {
     private final JdbcChatService jdbcChatService;
 
-    @PostMapping("/chats")
+    @PostMapping("/chat")
     @ResponseStatus(HttpStatus.CREATED)
     public void save(@RequestBody @Valid ChatRequest request) {
         jdbcChatService.save(request);
     }
 
-    @DeleteMapping("/chats/{id}")
+    @DeleteMapping("/chat/{chatId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Long id) {
-        jdbcChatService.delete(id);
+    public void delete(@PathVariable Long chatId) {
+        jdbcChatService.delete(chatId);
     }
 
-    @GetMapping("/chats/{id}")
-    public ChatResponse get(@PathVariable Long id) {
-        return jdbcChatService.get(id);
+    @GetMapping("/chat/{chatId}")
+    public Optional<ChatResponse> get(@PathVariable Long chatId) {
+        return jdbcChatService.get(chatId);
     }
 
     @GetMapping("/chats")

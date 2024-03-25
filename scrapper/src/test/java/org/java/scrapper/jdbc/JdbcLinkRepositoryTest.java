de7af1e92fe7f6a8e45c2c47ec8e88812d17ae4e
Mary Okhtienko <mariohtienko@gmail.com>
2024-03-25 21:53:05 +0300

    Update JdbcLinkRepositoryTest

--- scrapper/src/test/java/org/java/scrapper/jdbc/JdbcLinkRepositoryTest.java
+++ scrapper/src/test/java/org/java/scrapper/jdbc/JdbcLinkRepositoryTest.java
@@ -1,6 +1,7 @@
 package org.java.scrapper.jdbc;
 
 import org.java.scrapper.model.Link;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,6 +19,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @DirtiesContext
+@Disabled
 public class JdbcLinkRepositoryTest {
     @Autowired
     private JdbcLinkRepository jdbcLinkRepository;

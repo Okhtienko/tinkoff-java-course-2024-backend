b54b43563b2b4acb3d1f15f4a0b4e0ad35dd0334
Mary Okhtienko <mariohtienko@gmail.com>
2024-03-28 12:28:33 +0300

    Update JdbcLinkRepository

--- scrapper/src/main/java/org/java/scrapper/jdbc/JdbcLinkRepository.java
+++ scrapper/src/main/java/org/java/scrapper/jdbc/JdbcLinkRepository.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.java.scrapper.mapper.LinkMapper;
 import org.java.scrapper.model.Link;
 import org.java.scrapper.repository.LinkRepository;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -16,49 +15,69 @@ import org.springframework.stereotype.Repository;
 @Repository
 @RequiredArgsConstructor
 public class JdbcLinkRepository implements LinkRepository {
-    private final static String SQL_SAVE =
-        "INSERT INTO links (url, create_by, last_check, create_at) VALUES (?, ?, ?, ?)";
-    private final static String SQL_GETS =
-        "SELECT * FROM links INNER JOIN links_chats ON links.id = links_chats.link_id WHERE links_chats.chat_id = ?";
-
-    private final static String SQL_GET =
-        "SELECT * FROM links INNER JOIN links_chats ON links.id = links_chats.link_id "
-            + "WHERE links_chats.chat_id = ? AND links.url = ?";
-
-    private final static String SQL_REMOVE =
-        "DELETE FROM links WHERE id IN (SELECT link_id FROM links_chats WHERE chat_id = ?) AND url = ?";
-
-    private final static String SQL_EXISTS = "SELECT EXISTS(SELECT * FROM links WHERE url = ?)";
-
-    private final static String SQL_GET_LINK = "SELECT * FROM links WHERE url = ?";
-
-    private final static String SQL_SAVE_LINKS_CHATS = "INSERT INTO links_chats (link_id, chat_id) VALUES (?, ?)";
-
-    private final static String SQL_GETS_BY_LAST_CHECK = "SELECT * FROM links WHERE links.last_check < ?";
-
-    private static final String SQL_GET_CHATS_BY_LAST_CHECK =
-        "SELECT chat_id FROM links_chats INNER JOIN links ON links_chats.link_id = links.id "
-            + "WHERE links.url = ? AND links.last_check < ?";
-
-    private static final String SQL_UPDATE = "UPDATE links SET last_check = ? WHERE id = ?";
-
     private final JdbcTemplate jdbcTemplate;
     private final LinkMapper mapper;
 
-    @Value("${app.scheduler.force-check-delay}")
-    private Integer checkDelay;
+    private final static String SQL_REMOVE = "DELETE FROM links WHERE url = ?";
+
+    private final static String SQL_GET_LINK = "SELECT * FROM links WHERE url = ?";
+
+    private final static String SQL_GET =
+        """
+        SELECT * FROM links INNER JOIN links_chats ON links.id = links_chats.link_id
+        WHERE links_chats.chat_id = ? AND links.url = ?
+        """;
+
+    private final static String SQL_SAVE =
+        """
+        INSERT INTO links (url, create_by, last_check, create_at) VALUES (?, ?, ?, ?)
+        """;
+
+    private static final String SQL_GET_CHATS_BY_LAST_CHECK =
+        """
+        SELECT chat_id FROM links_chats INNER JOIN links ON links_chats.link_id = links.id
+        WHERE links.url = ? AND links.last_check < ?
+        ORDER BY links.last_check DESC LIMIT 100
+        """;
+
+    private static final String SQL_UPDATE = "UPDATE links SET last_check = ? WHERE id = ?";
+
+    private final static String SQL_GETS_BY_LAST_CHECK =
+        "SELECT * FROM links WHERE links.last_check < ? ORDER BY links.last_check DESC LIMIT 100";
+
+    private final static String SQL_EXISTS_BY_URL = "SELECT EXISTS(SELECT * FROM links WHERE url = ?)";
+
+    private final static String SQL_SAVE_LINKS_CHATS = "INSERT INTO links_chats (link_id, chat_id) VALUES (?, ?)";
+
+    private final static String SQL_GETS =
+        """
+        SELECT * FROM links INNER JOIN links_chats ON links.id = links_chats.link_id WHERE links_chats.chat_id = ?
+        """;
+
+    private final static String SQL_EXISTS_LINKS_CHATS = "SELECT EXISTS(SELECT * FROM links_chats WHERE link_id = ?)";
+
+    private final static String SQL_REMOVE_LINKS_CHATS = "DELETE FROM links_chats WHERE link_id = ? AND chat_id = ?";
+
+    private final static String SQL_EXISTS_BY_URL_CHAT_ID =
+        """
+        SELECT EXISTS(SELECT * FROM links WHERE id IN (SELECT link_id FROM links_chats WHERE chat_id = ?) AND url = ?)
+        """;
 
     @Override
     public Link save(String url, String createdBy, Long chatId) {
         log.info("Saving link: {}", url);
         jdbcTemplate.update(SQL_SAVE, url, createdBy, OffsetDateTime.now(), OffsetDateTime.now());
 
-        Link link = jdbcTemplate.queryForObject(SQL_GET_LINK, mapper, url);
-
-        jdbcTemplate.update(SQL_SAVE_LINKS_CHATS, link.getId(), chatId);
+        Link link = get(url);
+        updateLinkChat(link.getId(), chatId);
         log.info("Link saved with id: {}", link.getId());
-
         return link;
+    }
+
+    @Override
+    public Link get(String url) {
+        log.info("Getting link: {}", url);
+        return jdbcTemplate.queryForObject(SQL_GET_LINK, mapper, url);
     }
 
     @Override
@@ -68,11 +87,10 @@ public class JdbcLinkRepository implements LinkRepository {
     }
 
     @Override
-    public Link remove(String url, Long chatId) {
+    public Link remove(String url) {
+        log.info("Removing link: {}", url);
         Link link = jdbcTemplate.queryForObject(SQL_GET_LINK, mapper, url);
-
-        jdbcTemplate.update(SQL_REMOVE, chatId, url);
-
+        jdbcTemplate.update(SQL_REMOVE, url);
         log.info("Link removed: {}", url);
         return link;
     }
@@ -84,28 +102,55 @@ public class JdbcLinkRepository implements LinkRepository {
     }
 
     @Override
-    public List<Link> getsByLastCheck() {
-        return jdbcTemplate.query(SQL_GETS_BY_LAST_CHECK, mapper, OffsetDateTime.now().minusSeconds(checkDelay));
+    public List<Link> getsByLastCheck(Long delay) {
+        log.info("Getting links with last check before: {}", OffsetDateTime.now().minusSeconds(delay));
+        return jdbcTemplate.query(SQL_GETS_BY_LAST_CHECK, mapper, OffsetDateTime.now().minusSeconds(delay));
     }
 
     @Override
-    public List<Long> getsChatByLastCheck(String url) {
+    public List<Long> getsChatByLastCheck(Long delay, String url) {
+        log.info("Getting chats for link {} with last check before: {}", url, OffsetDateTime.now().minusSeconds(delay));
         return jdbcTemplate.query(
             SQL_GET_CHATS_BY_LAST_CHECK,
             (rs, rowNum) -> rs.getLong("chat_id"),
             url,
-            OffsetDateTime.now().minusSeconds(checkDelay)
+            OffsetDateTime.now().minusSeconds(delay)
         );
     }
 
     @Override
-    public Boolean exists(String url) {
-        log.info("Checking if link exists: {}", url);
-        return jdbcTemplate.queryForObject(SQL_EXISTS, Boolean.class, url);
+    public Boolean existsByUrl(String url) {
+        log.info("Checking existence of link: {}", url);
+        return jdbcTemplate.queryForObject(SQL_EXISTS_BY_URL, Boolean.class, url);
     }
 
     @Override
-    public void update(Long id) {
-        jdbcTemplate.update(SQL_UPDATE, Timestamp.from(OffsetDateTime.now().toInstant()), id);
+    public Boolean existsByUrlAndChatId(String url, Long chatId) {
+        log.info("Checking existence of link: {} for chat {}", url, chatId);
+        return jdbcTemplate.queryForObject(SQL_EXISTS_BY_URL_CHAT_ID, Boolean.class, chatId, url);
+    }
+
+    @Override
+    public Boolean existsLinkChat(Long id) {
+        log.info("Checking existence of link chat with id: {}", id);
+        return jdbcTemplate.queryForObject(SQL_EXISTS_LINKS_CHATS, Boolean.class, id);
+    }
+
+    @Override
+    public void updateLinkChat(Long id, Long chatId) {
+        log.info("Updating link chat: id={}, chatId={}", id, chatId);
+        jdbcTemplate.update(SQL_SAVE_LINKS_CHATS, id, chatId);
+    }
+
+    @Override
+    public void removeLinkChat(Long id, Long chatId) {
+        log.info("Removing link chat: id={}, chatId={}", id, chatId);
+        jdbcTemplate.update(SQL_REMOVE_LINKS_CHATS, id, chatId);
+    }
+
+    @Override
+    public void updateLastCheck(Long id, OffsetDateTime date) {
+        log.info("Updating last check for link: id={}, date={}", id, date);
+        jdbcTemplate.update(SQL_UPDATE, Timestamp.from(date.toInstant()), id);
     }
 }

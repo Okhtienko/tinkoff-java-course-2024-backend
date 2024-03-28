92a149bba10aa43dde8b58398807b582dc2311d8
Mary Okhtienko <mariohtienko@gmail.com>
2024-03-28 14:42:35 +0300

    Add LinkManagementService

---
+++ scrapper/src/main/java/org/java/scrapper/domain/LinkManagementService.java
@@ -0,0 +1,19 @@
+package org.java.scrapper.domain;
+
+import java.util.List;
+import org.java.scrapper.dto.link.LinkRequest;
+import org.java.scrapper.dto.link.LinkResponse;
+
+public interface LinkManagementService {
+    LinkResponse get(LinkRequest request, Long chatId);
+
+    LinkResponse save(LinkRequest request, Long chatId);
+
+    LinkResponse remove(LinkRequest request, Long chatId);
+
+    List<LinkResponse> gets(Long chatId);
+
+    List<LinkResponse> getsByLastCheck(Long delay);
+
+    List<Long> getsChatByLastCheck(Long delay, LinkResponse link);
+}
